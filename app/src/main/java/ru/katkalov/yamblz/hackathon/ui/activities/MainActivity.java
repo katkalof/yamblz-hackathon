c8cdd15d103895b970908da13ee092aaa24ea63d
user <katkalof@gmail.com>
2016-07-23 15:01:48 +0300

    initial commit

---
+++ app/src/main/java/ru/katkalov/yamblz/hackathon/ui/activities/MainActivity.java
@@ -0,0 +1,22 @@
+package ru.katkalov.yamblz.hackathon.ui.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import ru.katkalov.yamblz.hackathon.R;
+import ru.katkalov.yamblz.hackathon.ui.fragments.CollectWordFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.main_frame_layout, new CollectWordFragment())
+                    .commit();
+        }
+    }
+}
---
+++ app/src/main/java/ru/katkalov/yamblz/hackathon/ui/fragments/MainFragment.java
@@ -0,0 +1,17 @@
+package ru.katkalov.yamblz.hackathon.ui.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ru.katkalov.yamblz.hackathon.R;
+
+public class MainFragment extends android.support.v4.app.Fragment {
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
