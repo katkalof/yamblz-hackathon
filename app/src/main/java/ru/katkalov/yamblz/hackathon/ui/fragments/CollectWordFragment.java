964709d5c7264c96708a282bb222a3c8fd66d478
user <katkalof@gmail.com>
2016-07-23 19:34:57 +0300

    add fragment changing

--- app/src/main/java/ru/katkalov/yamblz/hackathon/ui/fragments/CollectWordFragment.java
+++ app/src/main/java/ru/katkalov/yamblz/hackathon/ui/fragments/CollectWordFragment.java
@@ -4,15 +4,40 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import ru.katkalov.yamblz.hackathon.R;
 
 public class CollectWordFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_collect_word, container, false);
+        final View view = inflater.inflate(R.layout.fragment_collect_word, container, false);
+
+
+        ImageView close = (ImageView) view.findViewById(R.id.close);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getFragmentManager();
+                Fragment fragment = fragmentManager.findFragmentByTag("MainFragment");
+                if (fragment == null) {
+                    fragmentManager
+                            .beginTransaction()
+                            .replace(R.id.main_frame_layout, new MainFragment(), "AboutFragment")
+                            .commit();
+                }
+                else {
+                    fragmentManager.beginTransaction().replace(R.id.main_frame_layout, fragment, "AboutFragment").commit();
+                }
+
+            }
+        });
+
+
+        return view;
     }
 }
