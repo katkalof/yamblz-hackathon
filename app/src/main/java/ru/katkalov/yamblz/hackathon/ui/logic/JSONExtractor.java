8965c0b949897d634b535c40c81eef2b2a6ac9c1
dsukmanova <sukmanovadarya@yandex.ru>
2016-07-23 15:29:40 +0300

    add words to assets, parse json to words array

---
+++ app/src/main/java/ru/katkalov/yamblz/hackathon/ui/logic/JSONExtractor.java
@@ -0,0 +1,51 @@
+package ru.katkalov.yamblz.hackathon.ui.logic;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by dsukmanova on 23.07.16.
+ */
+
+public class JSONExtractor {
+    InputStream is = null;
+
+    public JSONExtractor (InputStream is){
+        this.is = is;
+    }
+
+    public String[] getWordsFromJSon(String language) {
+        JSONObject jsonObject = null;
+
+        try {
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            jsonObject = new JSONObject(new String(buffer, "UTF-8"));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }catch (JSONException ex){
+            ex.printStackTrace();
+        }
+
+        JSONArray jWords;
+        String[] words = null;
+        if (jsonObject.has(language)) {
+            try {
+                jWords = jsonObject.getJSONArray(language);
+                words = new String[jWords.length()];
+                for (int i = 0; i < jWords.length(); i++) {
+                    words[i] = jWords.getString(i);
+                }
+            } catch (JSONException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return words;
+    }
+}
